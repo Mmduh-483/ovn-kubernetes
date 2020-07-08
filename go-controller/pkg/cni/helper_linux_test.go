2fba776dec40882997a4c92d50957b7b81a37cf0
Mamduh Alassi <mamduhala@mellanox.com>
2020-07-08 11:43:51 +0300

    Add smart nic cni Smart NIC CNI is used for smart-nic network devices, in smart nic the VF exists in the host machine where the pod will be running, but the VF representor exists on the SmartNic card itself which can be considerd as different host.
    The archecture of the SmartNic cards make a need for new CNI that
    can handle the SmartNIc different archecture.
    
    Signed-off-by: Mamduh Alassi <mamduhala@mellanox.com>

--- go-controller/pkg/cni/helper_linux_test.go
+++ go-controller/pkg/cni/helper_linux_test.go
@@ -574,6 +574,7 @@ func TestSetupSriovInterface(t *testing.T) {
 			errExp:      true,
 			sriovOpsMockHelper: []ovntest.TestifyMockHelper{
 				{"GetNetDevicesFromPci", []string{"string"}, []interface{}{[]string{"en01"}, nil}},
+				{"GetVfIndexByPciAddress", []string{"string"}, []interface{}{0, nil}},
 				{"GetUplinkRepresentor", []string{"string"}, []interface{}{"", fmt.Errorf("mock error")}},
 			},
 		},
@@ -590,7 +591,6 @@ func TestSetupSriovInterface(t *testing.T) {
 			errExp:      true,
 			sriovOpsMockHelper: []ovntest.TestifyMockHelper{
 				{"GetNetDevicesFromPci", []string{"string"}, []interface{}{[]string{"en01"}, nil}},
-				{"GetUplinkRepresentor", []string{"string"}, []interface{}{"testlinkrepresentor", nil}},
 				{"GetVfIndexByPciAddress", []string{"string"}, []interface{}{-1, fmt.Errorf("mock error")}},
 			},
 		},
